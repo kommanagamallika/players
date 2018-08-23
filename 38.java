df7ddafcfda0834b0323fc252073d7a042fd0d31
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-08-23 12:30:16 +0530

    Create 38.java

---
+++ 38.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int Number, i;
+	            Scanner sc = new Scanner(System.in);
+		Number = sc.nextInt();
+		for(i = 1; i <= Number; i++) 
+		{
+			if(Number%i == 0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
